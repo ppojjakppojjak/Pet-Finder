935ba69907ccd441e3624344c6b7f070d8823187
MinHyung Chae <fkdlskmin1@gmail.com>
2018-06-29 20:36:48 +0900

    all satisfied / cat finish!!!!!!!!

---
+++ app/src/main/java/com/ppojjakppojjak/ppojjakppojjak/TabFragment3_cat.java
@@ -0,0 +1,15 @@
+package com.ppojjakppojjak.ppojjakppojjak;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TabFragment3_cat extends Fragment  {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_fragment_3_cat, container, false);
+    }
+}
